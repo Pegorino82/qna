dfc3f137b6f5ab67f004b98b42a793be0db95bc8
Pegorino82 <eshkryabin@yandex.ru>
2021-12-21 18:13:46 +0300

    Add answers create controller with specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show edit update destroy]
 
@@ -5,15 +7,13 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @question = Question.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @question = Question.new(question_params)
