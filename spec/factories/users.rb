406119c25f2828a3174c302dabac4588188c0843
Pegorino82 <eshkryabin@yandex.ru>
2021-12-22 16:17:31 +0300

    Refactoring acceptance test for user log in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@mail.com"
+  end
+
   factory :user do
-    
+    email
+    password { 'qwerty' }
+    password_confirmation { 'qwerty' }
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,7 +6,7 @@ feature 'User can sign in', %q{
   I'd like to be able to sign in
 } do
 
-  given(:user) { User.create!(email: 'user@test.com', password: 'qwerty') }
+  given(:user) { create :user }
 
   background { visit new_user_session_path }
 
@@ -14,9 +14,7 @@ feature 'User can sign in', %q{
     #  capybara methods below
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    # click_button 'Log in'
-    click_on 'Log in'
-    # click_button t('devise...')
+    click_on I18n.t('devise.sessions.new.log_in')
 
     # save_and_open_page
     expect(page).to have_content I18n.t('devise.sessions.signed_in')
@@ -25,7 +23,7 @@ feature 'User can sign in', %q{
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'bad@test.com'
     fill_in 'Password', with: 'qwerty'
-    click_button 'Log in'
+    click_on I18n.t('devise.sessions.new.log_in')
 
     expect(page).to have_content I18n.t('devise.failure.invalid', authentication_keys: 'Email')
   end
