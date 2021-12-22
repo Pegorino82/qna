fce36d59a570fe5a5cb6746d2d47c8ce78fbde6a
Pegorino82 <eshkryabin@yandex.ru>
2021-12-22 18:03:54 +0300

    Acceptance test fo user sign out

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,19 +8,16 @@ feature 'User can sign in', %q{
 
   given(:user) { create :user }
 
-  background { visit new_user_session_path }
-
   scenario 'Registered user tries to sign in' do
-    #  capybara methods below
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on I18n.t('devise.sessions.new.log_in')
+    sign_in(user)
 
     # save_and_open_page
     expect(page).to have_content I18n.t('devise.sessions.signed_in')
   end
 
   scenario 'Unregistered user tries to sign in' do
+    visit new_user_session_path
+
     fill_in 'Email', with: 'bad@test.com'
     fill_in 'Password', with: 'qwerty'
     click_on I18n.t('devise.sessions.new.log_in')
---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  As an unauthenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) { create :user }
+
+  scenario 'Authenticated user tries to sign out' do
+    sign_in(user)
+
+    click_on I18n.t('main_nav.sign_out')
+
+    expect(page).to have_content I18n.t('devise.sessions.signed_out')
+  end
+end
