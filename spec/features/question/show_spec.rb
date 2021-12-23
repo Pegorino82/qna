704c2257fe6df2f7685d245fd175f2372b8db479
Pegorino82 <eshkryabin@yandex.ru>
2021-12-23 15:51:18 +0300

    Acceptance test for show of questions

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can view question and it answers', "
+  In order to view a question
+  As an user
+  I'd like to be able to see the question and it answers
+" do
+  given(:question) { create :question }
+  given!(:answers) { create_list :answer, 3, question: question }
+
+  describe 'Authenticated user' do
+    scenario 'can see the question and it answers' do
+      user = create :user
+
+      sign_in(user)
+
+      visit question_path(question)
+
+      expect(page).to have_content I18n.t('questions.show.header')
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+
+      expect(page).to have_content I18n.t('questions.show.answers')
+      expect(page.all('li').count).to eq 3
+    end
+  end
+
+  scenario 'Unauthenticated user can the question and it answers' do
+    visit question_path(question)
+
+    expect(page).to have_content I18n.t('questions.show.header')
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    expect(page).to have_content I18n.t('questions.show.answers')
+    expect(page.all('li').count).to eq 3
+  end
+end
