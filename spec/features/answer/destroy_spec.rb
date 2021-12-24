25e1b75a8becfe571a3da00758886f69606a9f44
Pegorino82 <eshkryabin@yandex.ru>
2021-12-24 15:33:22 +0300

    Acceptance test unauth user cannot delete answers

--- spec/features/answer/destroy_spec.rb
+++ spec/features/answer/destroy_spec.rb
@@ -9,27 +9,38 @@ feature 'Authenticated user can delete his answer', "
 " do
   given!(:user) { create :user }
 
-  background { sign_in(user) }
+  context 'Authenticated user' do
+    background { sign_in(user) }
 
-  scenario 'Authenticated user can delete his question' do
-    question = create :question, author: user
-    answer = create :answer, question: question, author: user
+    scenario 'can delete his answer' do
+      question = create :question, author: user
+      answer = create :answer, question: question, author: user
 
-    visit question_path(question)
+      visit question_path(question)
 
-    click_on I18n.t('questions.show.delete_answer')
+      click_on I18n.t('questions.show.delete_answer')
 
-    expect(page).to have_content I18n.t('answers.destroy.success')
-    expect(page).to have_content I18n.t('questions.show.answers')
-    expect(page).to_not have_content answer.body
+      expect(page).to have_content I18n.t('answers.destroy.success')
+      expect(page).to have_content I18n.t('questions.show.answers')
+      expect(page).to_not have_content answer.body
+    end
+
+    scenario 'can not delete others answer' do
+      other_user = create :user
+      others_question = create :question, author: other_user
+      create :answer, question: others_question, author: other_user
+
+      visit question_path(others_question)
+
+      expect(page).to_not have_content I18n.t('questions.show.delete_answer')
+    end
   end
 
-  scenario 'Authenticated user can delete others question' do
-    other_user = create :user
-    others_question = create :question, author: other_user
-    create :answer, question: others_question, author: other_user
+  scenario 'Unauthenticated user can not delete answer' do
+    question = create :question, author: user
+    create :answer, question: question, author: user
 
-    visit question_path(others_question)
+    visit question_path(question)
 
     expect(page).to_not have_content I18n.t('questions.show.delete_answer')
   end
