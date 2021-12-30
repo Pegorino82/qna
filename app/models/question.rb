c0ad84be7dd512c5402fbbc30e47645c8928458f
Pegorino82 <eshkryabin@yandex.ru>
2021-12-30 10:12:21 +0300

    Can create question with many files

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
-  has_one_attached :file
+  has_many_attached :files
 
   belongs_to :best_answer, class_name: 'Answer', optional: true
   belongs_to :author, class_name: 'User'
