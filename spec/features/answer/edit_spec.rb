9f18d8da5d020a82f1eb37913084ba75ea3376d7
Pegorino82 <eshkryabin@yandex.ru>
2022-01-04 12:16:57 +0300

    Refactoring

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -12,70 +12,76 @@ feature 'Authenticated user can edit his answer', "
   given!(:answer) { create :answer, question: question, author: user }
 
   describe 'Authenticated user', js: true do
-    background do
-      sign_in(user)
+    background { sign_in(user) }
 
-      visit question_path(question)
-    end
+    context 'is author' do
+      background do
+        visit question_path(question)
 
-    scenario 'edit his answer' do
-      click_on I18n.t('questions.show.edit_answer')
-
-      within '.answers' do  # to be sure we are looking for answer in answers
-        fill_in 'Body', with: 'Edited answer'
-        click_button I18n.t('answers.edit.submit')
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'Edited answer'
-        expect(page).to_not have_selector 'textarea'
-      end
-    end
-
-    scenario 'add files when edit his answer' do
-      click_on I18n.t('questions.show.edit_answer')
-
-      within '.answers' do
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_button I18n.t('answers.edit.submit')
-
-        expect(page).to have_link 'rails_helper.rb'
-        expect(page).to have_link 'spec_helper.rb'
-      end
-    end
-
-    scenario 'can delete file', js: true do
-      click_on I18n.t('questions.show.edit_answer')
-
-      within '.answers' do
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_on I18n.t('answers.edit.submit')
-
-        find("#answer_#{answer.id}_files").first(:link, I18n.t('files.destroy.delete')).click
-
-        expect(page).to_not have_link 'rails_helper.rb'
-      end
-    end
-
-    scenario 'edit his answer with errors' do
-      within '.answers' do  # to be sure we are looking for answer in answers
         click_on I18n.t('questions.show.edit_answer')
+      end
 
-        fill_in 'Body', with: ''
+      scenario 'edit his answer' do
+        within '.answers' do
+          # to be sure we are looking for answer in answers
+          fill_in 'Body', with: 'Edited answer'
+          click_button I18n.t('answers.edit.submit')
 
-        click_button I18n.t('answers.edit.submit')
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'Edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
 
-        expect(page).to have_content "Body can't be blank"
+      scenario 'add files when edit his answer' do
+        within '.answers' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_button I18n.t('answers.edit.submit')
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario 'can delete file' do
+        within '.answers' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on I18n.t('answers.edit.submit')
+
+          find("#answer_#{answer.id}_files").first(:link, I18n.t('files.destroy.delete')).click
+
+          expect(page).to_not have_link 'rails_helper.rb'
+        end
+      end
+
+      scenario 'edit his answer with errors' do
+        within '.answers' do
+          # to be sure we are looking for answer in answers
+          fill_in 'Body', with: ''
+          click_button I18n.t('answers.edit.submit')
+
+          expect(page).to have_content "Body can't be blank"
+        end
       end
     end
 
-    scenario "tries to edit other's answer" do
-      other_user = create :user
-      other_question = create :question, author: other_user
-      create :answer, question: other_question, author: other_user
+    context 'is not author' do
+      given!(:other_user) { create :user }
+      given!(:other_question) { create :question, author: other_user }
+      given!(:file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb", 'text/plain') }
+      given!(:other_answer) { create :answer, question: other_question, author: other_user, files: [file] }
 
-      visit question_path(other_question)
+      background { visit question_path(other_question) }
 
-      expect(page).to_not have_content I18n.t('questions.show.edit_answer')
+      scenario "tries to edit other's answer" do
+        expect(page).to_not have_content I18n.t('questions.show.edit_answer')
+      end
+
+      scenario "tries to delete other's file" do
+        within ".answers div[id^='answer']" do
+          expect(page).to_not have_link I18n.t('files.destroy.delete')
+        end
+      end
     end
   end
 
--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -11,77 +11,93 @@ feature 'Authenticated user can edit his question', "
   given!(:question) { create :question, author: user }
 
   describe 'Authenticated user' do
-    background do
-      sign_in(user)
+    background { sign_in(user) }
 
-      visit question_path(question)
-    end
+    context 'is author', js: true do
+      background do
+        visit question_path(question)
 
-    scenario 'edit his question', js: true do
-      click_on I18n.t('questions.show.edit_answer')
+        click_on I18n.t('questions.show.edit')
+      end
 
-      within '.question' do
-        fill_in 'Title', with: 'Edited Title'
-        fill_in 'Body', with: 'Edited Body'
-        click_on I18n.t('answers.edit.submit')
+      scenario 'edit question' do
+        within '.question' do
+          fill_in 'Title', with: 'Edited Title'
+          fill_in 'Body', with: 'Edited Body'
+          click_on I18n.t('questions.edit.submit')
 
-        expect(page).to_not have_content question.title
-        expect(page).to_not have_content question.body
-        expect(page).to_not have_selector 'textarea'
+          expect(page).to_not have_content question.title
+          expect(page).to_not have_content question.body
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'add files when edit question' do
+        within '.question' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on I18n.t('questions.edit.submit')
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario 'can delete file' do
+        within '.question' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on I18n.t('questions.edit.submit')
+
+          find('#question_files').first(:link, I18n.t('files.destroy.delete')).click
+
+          expect(page).to_not have_link 'rails_helper.rb'
+        end
+      end
+
+      scenario 'edit his question with errors' do
+        within '.question' do
+          fill_in 'Title', with: ''
+          click_on I18n.t('questions.edit.submit')
+
+          expect(page).to have_content "Title can't be blank"
+        end
       end
     end
 
-    scenario 'add files when edit his question', js: true do
-      click_on I18n.t('questions.show.edit_answer')
+    context 'is not author' do
+      given!(:other_user) { create :user }
+      given!(:other_question) { create :question, author: other_user }
 
-      within '.question' do
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_on I18n.t('answers.edit.submit')
-
-        expect(page).to have_link 'rails_helper.rb'
-        expect(page).to have_link 'spec_helper.rb'
+      background { visit question_path(other_question) }
+      scenario "tries to edit other's question" do
+        within '.question' do
+          expect(page).to_not have_link I18n.t('questions.show.edit')
+        end
       end
-    end
 
-    scenario 'can delete file', js: true do
-      click_on I18n.t('questions.show.edit_answer')
+      scenario "tries to delete other's file" do
+        file = fixture_file_upload("#{Rails.root}/spec/rails_helper.rb", 'text/plain')
+        other_question.files.attach(file)
 
-      within '.question' do
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_on I18n.t('answers.edit.submit')
-
-        find('#question_files').first(:link, I18n.t('files.destroy.delete')).click
-
-        expect(page).to_not have_link 'rails_helper.rb'
-      end
-    end
-
-    scenario 'edit his answer with errors', js: true do
-      click_on I18n.t('questions.show.edit_answer')
-
-      within '.question' do
-        fill_in 'Title', with: ''
-        click_on I18n.t('answers.edit.submit')
-
-        expect(page).to have_content "Title can't be blank"
-      end
-    end
-
-    scenario "tries to edit other's answer" do
-      other_user = create :user
-      other_question = create :question, author: other_user
-
-      visit question_path(other_question)
-
-      within '.question' do
-        expect(page).to_not have_link I18n.t('questions.show.edit_answer')
+        within '.question > #question_files' do
+          expect(page).to_not have_link I18n.t('files.destroy.delete')
+        end
       end
     end
   end
 
-  scenario 'Unauthenticated user can not edit answer' do
-    visit question_path(question)
+  describe 'Unauthenticated user' do
+    scenario 'can not edit question' do
+      visit question_path(question)
 
-    expect(page).to_not have_link 'Edit'
+      expect(page).to_not have_link I18n.t('questions.show.edit')
+    end
+
+    scenario 'can not delete file' do
+      visit question_path(question)
+
+      within '.question > #question_files' do
+        expect(page).to_not have_link I18n.t('files.destroy.delete')
+      end
+    end
   end
 end
