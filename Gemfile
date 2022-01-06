e7693f2df0bc9317f5a6c448149acc0c3efa299e
Pegorino82 <eshkryabin@yandex.ru>
2022-01-06 20:10:32 +0300

    Can add several links to question

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem "aws-sdk-s3", require: false
 gem "sassc-rails"
 gem 'bootstrap', '~> 5.1.3'
 gem 'popper_js', '~> 2.9.3'
+gem 'jquery-rails'
+gem 'cocoon'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -10,23 +10,41 @@ feature 'User can add links to question', "
   given(:user) { create :user }
   given(:gist_url) { 'https://gist.github.com/Pegorino82/8f318de6098f1db26020d38a809a3b75' }
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     background do
       sign_in(user)
 
       visit new_question_path
-    end
 
-    scenario 'can add link when asks question' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'Test body'
 
+      click_on I18n.t('links.new.add')
+
       fill_in 'Link title', with: 'My gist'
       fill_in 'Url', with: gist_url
+    end
+
+    scenario 'can add link when asks question' do
+      click_on I18n.t('questions.new.ask')
+
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+
+    scenario 'can add several links when asks question' do
+      google_url = 'https://google.com'
+
+      click_on I18n.t('links.new.add')
+
+      within all('.nested_fields').last do
+        fill_in 'Link title', with: 'Google'
+        fill_in 'Url', with: google_url
+      end
 
       click_on I18n.t('questions.new.ask')
 
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
     end
   end
 end
