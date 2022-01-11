e46064d91210b065dfbc79b712624e2cc06872f3
Pegorino82 <eshkryabin@yandex.ru>
2022-01-11 19:31:56 +0300

    Model concern for files

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
+  include Attachable
+
   belongs_to :question
   belongs_to :author, class_name: 'User'
 
   has_many :links, dependent: :destroy, as: :linkable
   has_one :award
 
-  has_many_attached :files
   accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Attachable
+
   has_many :answers, dependent: :destroy
   has_many :links, dependent: :destroy, as: :linkable
   has_one :award, dependent: :destroy
 
-  has_many_attached :files
   accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :award, reject_if: :all_blank
 
