d78575470e2b76f058c0521fc4c6267928b760f2
Pegorino82 <eshkryabin@yandex.ru>
2022-01-27 17:25:46 +0300

    Add Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,29 @@ class User < ApplicationRecord
   has_many :awards, through: :answers
   has_many :votes, class_name: 'Vote', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+  has_many :authorizations, dependent: :destroy
 
   def author_of?(resource)
     id == resource.author_id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info&.email
+    user = User.find_by(email: email)
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_175334) do
+ActiveRecord::Schema.define(version: 2022_01_27_134110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2022_01_25_175334) do
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_answers_on_author_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "awards", force: :cascade do |t|
@@ -123,6 +132,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_175334) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "awards", "answers"
   add_foreign_key "awards", "questions"
   add_foreign_key "comments", "users", column: "author_id"
