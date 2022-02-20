bceb82182f61f16048a5331f3e82f24a7bbb3517
Pegorino82 <eshkryabin@yandex.ru>
2022-02-20 18:20:08 +0300

    Spec for search controller

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -10,17 +10,28 @@ RSpec.describe SearchController, type: :controller do
     before { login(user) }
 
     context 'with valid attributes' do
+      before { get :search, params: { text: 'test', question: '1', answer: '1', comment: '1', user: '1' } }
+
       it 'populates params' do
-        get :search, params: { text: 'test', question: '1', answer: '1', comment: '1', user: '1' }
-        # expect(assigns(:search_params)).to eq '{"text"=>"test", "question"=>"1", "answer"=>"1", "comment"=>"1", "user"=>"1"}'
+        search_params = {"text"=>"test", "question"=>"1", "answer"=>"1",
+                         "comment"=>"1", "user"=>"1", "action"=>"search", "controller"=>"search"}
+        expect(assigns(:search_params)).to eq search_params
       end
 
-      it 'renders search view'
+      it 'renders search view' do
+        expect(response).to render_template 'search/_result'
+      end
     end
 
     context 'with invalid attributes' do
-      it 'does not populates params'
-      it 'renders search view'
+      before { get :search, params: { text: '', question: '1', answer: '1', comment: '1', user: '1' } }
+
+      it 'does not populates params' do
+        expect(assigns(:results)).to_not be
+      end
+      it 'renders search view' do
+        expect(response).to render_template 'search/_result'
+      end
     end
   end
 end
